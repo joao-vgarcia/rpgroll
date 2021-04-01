ee65aed3e578c57dd880a6e7055c98bda991e7b0
João Vitor Garcia <57414096+joao-vgarcia@users.noreply.github.com>
2021-04-01 18:11:26 -0300

    set always portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rpgroll/get_it/get_it.dart';
 import 'package:rpgroll/modules/home/home_screen.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   setUpLocator();
   runApp(MyApp());
 }
@@ -11,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Dice Roller',
+        title: 'RPG Roller',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           visualDensity: VisualDensity.adaptivePlatformDensity,
